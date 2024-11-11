14224027d112460ba4b91516a1031056194c7948
Bryan Galecio <bryangalecio2004@gmail.com>
2024-11-10 22:01:55 -0500

    Update 6502_Read.ino

--- 6502_Read.ino
+++ 6502_Read.ino
@@ -1,24 +1,25 @@
+// Arduino program used for observing computer behavior and for troubleshooting
 const char ADDR[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52};
 const char DATA[] = {39, 41, 43, 45, 47, 49, 51, 53};
 #define CLOCK 2
 #define READ_WRITE 3
 
 void setup() {
-  for (int n = 0; n < 16; n += 1) {
+  for (int n = 0; n < 16; n += 1) { // Initialize Adress bits to Arduino pins
     pinMode(ADDR[n], INPUT);
   }
-  for (int n = 0; n < 8; n += 1) {
+  for (int n = 0; n < 8; n += 1) { // Initialize Data bits to Arduino pins
     pinMode(DATA[n], INPUT);
   }
   pinMode(CLOCK, INPUT);
   pinMode(READ_WRITE, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
+  attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING); // Calls onClock function when a rising edge is detected
   
   Serial.begin(57600);
 }
 
-void onClock() {
+void onClock() { // Prints Data bits and Address bits in binary and in hex, and outputs whether the CPU is reading or writing
   char output[15];
 
   unsigned int address = 0;
